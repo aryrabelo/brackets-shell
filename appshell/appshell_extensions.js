6f030984db97aa470c384744416c2190e52134ba
JeffryBooher <jsbooher@adobe.com>
2013-08-14 09:30:23 -0700

    add new APIs for FS work

--- appshell/appshell_extensions.js
+++ appshell/appshell_extensions.js
@@ -277,6 +277,8 @@ if (!appshell.app) {
         }, path);
     };
  
+ 
+ 
     /**
      * Quits native shell application
      */
@@ -419,6 +421,40 @@ if (!appshell.app) {
         MoveFileOrDirectoryToTrash(callback || _dummyCallback, path);
     };    
 
+    /**
+     * Copy src to dest, replacing the file at dest if it already exists.
+     *
+     * @param {string} path The path of the file to copy.
+     * @param {string} data The destination to copy the file to
+     * @param {function(err)} callback Asynchronous callback function. The callback gets one argument (err).
+     *        Possible error values:
+     *          NO_ERROR
+     *          ERR_UNKNOWN
+     *          ERR_INVALID_PARAMS
+     *          ERR_UNSUPPORTED_ENCODING
+     *          ERR_OUT_OF_SPACE
+     *
+     * @return None. This is an asynchronous call that sends all return information to the callback.
+     */
+    native function CopyFile();
+    appshell.fs.copyFile = function (src, dest, callback) {
+        CopyFile(callback || _dummyCallback, src, dest);
+    };
+ 
+
+    /** Return the home directory as the second argument of the callback function */
+    native function GetHomeDir();
+    appshell.fs.getHomeDir = function (callback) {
+        GetHomeDir(callback);
+    };
+ 
+    /** Return the home directory as the second argument of the callback function */
+    native function GetDocumentsDir();
+    appshell.fs.getHomeDir = function (callback) {
+        GetDocumentsDir(callback);
+    };
+ 
+ 
     /**
      * Return the number of milliseconds that have elapsed since the application
      * was launched. 
